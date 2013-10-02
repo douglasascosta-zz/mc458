22930acccdccca1dfb4b8616de8855bbfd8ebe3e
Samuel Birocchi <samuelbirocchi@gmail.com>
2013-10-02 16:48:44 -0300

    Extração de dados concluido.

--- mc458-trab1/src/geraVetores.cpp
+++ mc458-trab1/src/geraVetores.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,20 +25,21 @@ void bubbleInverso(int *v, int n) {
 	}
 }
 
-void geraBubble(){
+void geraBubble() {
 	FILE *Bubble;
 
 	Bubble = fopen("Bubble_M.txt", "wt");
 	int *v;
 
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < 10; i++) {
 		v = (int*) malloc(100 * sizeof(int));
-		for (int j = 0; j < 100; j++){
-			v[j] = rand()%MAX_INT;
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT
+			;
 		}
 		quick(v, 0, 99);
 		fprintf(Bubble, "100");
-		for (int j = 0; j < 100; j++){
+		for (int j = 0; j < 100; j++) {
 			fprintf(Bubble, ",%d", v[j]);
 		}
 		fprintf(Bubble, ";\n");
@@ -48,36 +48,38 @@ void geraBubble(){
 	fclose(Bubble);
 
 	Bubble = fopen("Bubble_P.txt", "wt");
-	for (int i = 0; i < 10; i++){
-			v = (int*) malloc(100 * sizeof(int));
-			for (int j = 0; j < 100; j++){
-				v[j] = rand()%MAX_INT;
-			}
-			bubbleInverso(v, 100);
-			fprintf(Bubble, "100");
-			for (int j = 0; j < 100; j++){
-				fprintf(Bubble, ",%d", v[j]);
-			}
-			fprintf(Bubble, ";\n");
-			free(v);
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT
+			;
 		}
-		fclose(Bubble);
+		bubbleInverso(v, 100);
+		fprintf(Bubble, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Bubble, ",%d", v[j]);
+		}
+		fprintf(Bubble, ";\n");
+		free(v);
+	}
+	fclose(Bubble);
 }
 
-void geraInsertion(){
+void geraInsertion() {
 	FILE *Insertion;
 
 	Insertion = fopen("Insertion_M.txt", "wt");
 	int *v;
 
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < 10; i++) {
 		v = (int*) malloc(100 * sizeof(int));
-		for (int j = 0; j < 100; j++){
-			v[j] = rand()%MAX_INT;
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT
+			;
 		}
 		quick(v, 0, 99);
 		fprintf(Insertion, "100");
-		for (int j = 0; j < 100; j++){
+		for (int j = 0; j < 100; j++) {
 			fprintf(Insertion, ",%d", v[j]);
 		}
 		fprintf(Insertion, ";\n");
@@ -86,36 +88,75 @@ void geraInsertion(){
 	fclose(Insertion);
 
 	Insertion = fopen("Insertion_P.txt", "wt");
-	for (int i = 0; i < 10; i++){
-			v = (int*) malloc(100 * sizeof(int));
-			for (int j = 0; j < 100; j++){
-				v[j] = rand()%MAX_INT;
-			}
-			bubbleInverso(v, 100);
-			fprintf(Insertion, "100");
-			for (int j = 0; j < 100; j++){
-				fprintf(Insertion, ",%d", v[j]);
-			}
-			fprintf(Insertion, ";\n");
-			free(v);
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT
+			;
 		}
-		fclose(Insertion);
+		bubbleInverso(v, 100);
+		fprintf(Insertion, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Insertion, ",%d", v[j]);
+		}
+		fprintf(Insertion, ";\n");
+		free(v);
+	}
+	fclose(Insertion);
 }
 
-void geraAleatorios(){
+void geraMerge() {
+	FILE *Merge;
+
+	Merge = fopen("Merge_M.txt", "wt");
+	int *v;
+
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT;
+		}
+		quick(v, 0, 99);
+		fprintf(Merge, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Merge, ",%d", v[j]);
+		}
+		fprintf(Merge, ";\n");
+		free(v);
+	}
+	fclose(Merge);
+
+	Merge = fopen("Merge_P.txt", "wt");
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = 1*(j*i+1)+100*(j*i+1)*(j%2);
+		}
+		fprintf(Merge, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Merge, ",%d", v[j]);
+		}
+		fprintf(Merge, ";\n");
+		free(v);
+	}
+	fclose(Merge);
+}
+
+void geraAleatorios() {
 	FILE *Aleatorios;
 
 	Aleatorios = fopen("Aleatorios.txt", "wt");
 	int *v;
 
-	for (int i = 0; i < 50; i++){
-		int s = 20*(i+1);
+	for (int i = 0; i < 50; i++) {
+		int s = 20 * (i + 1);
 		v = (int*) malloc(s * sizeof(int));
-		for (int j = 0; j < s; j++){
-			v[j] = rand()%MAX_INT;
+		for (int j = 0; j < s; j++) {
+			v[j] = rand() % MAX_INT
+			;
 		}
 		fprintf(Aleatorios, "%d", s);
-		for (int j = 0; j < s; j++){
+		for (int j = 0; j < s; j++) {
 			fprintf(Aleatorios, ",%d", v[j]);
 		}
 		fprintf(Aleatorios, ";\n");
@@ -123,3 +164,81 @@ void geraAleatorios(){
 	}
 	fclose(Aleatorios);
 }
+
+void geraQuick() {
+	FILE *Quick;
+
+	Quick = fopen("Quick_M.txt", "wt");
+	int *v;
+
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT;
+		}
+		fprintf(Quick, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Quick, ",%d", v[j]);
+		}
+		fprintf(Quick, ";\n");
+		free(v);
+	}
+	fclose(Quick);
+
+	Quick = fopen("Quick_P.txt", "wt");
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT
+			;
+		}
+		bubbleInverso(v, 100);
+		fprintf(Quick, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Quick, ",%d", v[j]);
+		}
+		fprintf(Quick, ";\n");
+		free(v);
+	}
+	fclose(Quick);
+}
+
+void geraHeap() {
+	FILE *Heap;
+
+	Heap = fopen("Heap_P.txt", "wt");
+	int *v;
+
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT;
+		}
+		fprintf(Heap, "100");
+		quick(v, 0, 99);
+		for (int j = 0; j < 100; j++) {
+			fprintf(Heap, ",%d", v[j]);
+		}
+		fprintf(Heap, ";\n");
+		free(v);
+	}
+	fclose(Heap);
+
+	Heap = fopen("Heap_M.txt", "wt");
+	for (int i = 0; i < 10; i++) {
+		v = (int*) malloc(100 * sizeof(int));
+		for (int j = 0; j < 100; j++) {
+			v[j] = rand() % MAX_INT;
+		}
+		bubbleInverso(v, 100);
+		fprintf(Heap, "100");
+		for (int j = 0; j < 100; j++) {
+			fprintf(Heap, ",%d", v[j]);
+		}
+		fprintf(Heap, ";\n");
+		free(v);
+	}
+	fclose(Heap);
+}
+
+
--- mc458-trab1/src/main.cpp
+++ mc458-trab1/src/main.cpp
@@ -21,6 +21,7 @@
 #include <ctime>
 #include <fstream>
 #include <string>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -32,6 +33,9 @@ void heap(int*, int);
 void geraBubble();
 void geraAleatorios();
 void geraInsertion();
+void geraMerge();
+void geraQuick();
+void geraHeap();
 vector<string> &split(const string &s, char delim, vector<string> &elems) {
 	stringstream ss(s);
 	string item;
@@ -51,14 +55,12 @@ void printV(int* v, int n);
 int main(int argc, const char* argv[]) {
 
 	vector<string> temp;
-	clock_t start;
-	double duration;
 	ifstream infile;
 	string line;
 	int* v;
 	int i = 0;
+	timeval start, end;
 
-	start = clock();
 	int n = 100;
 
 	if (argc <= 2) {
@@ -66,87 +68,88 @@ int main(int argc, const char* argv[]) {
 		return 0;
 	}
 
-	if (!strcmp(argv[2], "-bm")) {
-		infile.open("Bubble_M.txt");
-	} else if (!strcmp(argv[2], "-bp")) {
-		infile.open("Bubble_P.txt");
-	} else if (!strcmp(argv[2], "-im")) {
-		infile.open("Insertion_M.txt");
-	} else if (!strcmp(argv[2], "-ip")) {
-		infile.open("Insertion_P.txt");
-	} else if (!strcmp(argv[2], "-mm")) {
-		infile.open("Merge_M.txt");
-	} else if (!strcmp(argv[2], "-mp")) {
-		infile.open("Merge_P.txt");
-	} else if (!strcmp(argv[2], "-qm")) {
-		infile.open("Quick_M.txt");
-	} else if (!strcmp(argv[2], "-qp")) {
-		infile.open("Quick_P.txt");
-	} else if (!strcmp(argv[2], "-hm")) {
-		infile.open("Heap_M.txt");
-	} else if (!strcmp(argv[2], "-hp")) {
-		infile.open("Heap_P.txt");
-	} else {
-		infile.open("Aleatorios.txt");
+	for (int z = 0; z < 100; z++) {
+
+		if (!strcmp(argv[2], "-bm")) {
+			infile.open("Bubble_M.txt");
+		} else if (!strcmp(argv[2], "-bp")) {
+			infile.open("Bubble_P.txt");
+		} else if (!strcmp(argv[2], "-im")) {
+			infile.open("Insertion_M.txt");
+		} else if (!strcmp(argv[2], "-ip")) {
+			infile.open("Insertion_P.txt");
+		} else if (!strcmp(argv[2], "-mm")) {
+			infile.open("Merge_M.txt");
+		} else if (!strcmp(argv[2], "-mp")) {
+			infile.open("Merge_P.txt");
+		} else if (!strcmp(argv[2], "-qm")) {
+			infile.open("Quick_M.txt");
+		} else if (!strcmp(argv[2], "-qp")) {
+			infile.open("Quick_P.txt");
+		} else if (!strcmp(argv[2], "-hm")) {
+			infile.open("Heap_M.txt");
+		} else if (!strcmp(argv[2], "-hp")) {
+			infile.open("Heap_P.txt");
+		} else {
+			infile.open("Aleatorios.txt");
+		}
+
+		while (getline(infile, line)) {
+
+			if (!strcmp(argv[2], "-a")) {
+				n = 20 * (i + 1);
+			}
+			v = (int*) malloc(n * sizeof(int));
+			temp = split(line, ',');
+
+			for (int j = 1; j < n; j++) {
+				v[j - 1] = atoi(temp.at(j).c_str());
+			}
+
+			gettimeofday(&start, NULL);
+			if (!strcmp(argv[1], "-b")) {
+				bubble(v, n);
+			}
+			if (!strcmp(argv[1], "-i")) {
+				insertion(v, n);
+			}
+			if (!strcmp(argv[1], "-m")) {
+				merge(v, 0, n - 1);
+			}
+			if (!strcmp(argv[1], "-q")) {
+				quick(v, 0, n - 1);
+			}
+			if (!strcmp(argv[1], "-h")) {
+				heap(v, n);
+			}
+			if (!strcmp(argv[1], "-g")) {
+				geraBubble();
+				geraAleatorios();
+				geraInsertion();
+				geraMerge();
+				geraQuick();
+				geraHeap();
+				break;
+			}
+
+			free(v);
+			i++;
+			if (!strcmp(argv[2], "-a")) {
+				gettimeofday(&end, NULL);
+				printf("Tempo %d: %ld us\n", n, end.tv_usec - start.tv_usec);
+				gettimeofday(&start, NULL);
+			}
+
+		}
+		gettimeofday(&end, NULL);
+
+		suseconds_t time = end.tv_usec - start.tv_usec;
+
+		infile.close();
+
+		printf("Tempo: %ld us\n", time);
+
 	}
-
-	while (getline(infile, line)) {
-		printf("Antes %d:\n", i + 1);
-
-		if (!strcmp(argv[2], "-a")) {
-			n = 20 * (i + 1);
-		}
-		v = (int*) malloc(n * sizeof(int));
-		temp = split(line, ',');
-
-		for (int j = 1; j < n; j++) {
-			v[j] = atoi(temp.at(j).c_str());
-		}
-		printV(v, n);
-		//Bubble sort
-		if (!strcmp(argv[1], "-b")) {
-			bubble(v, n);
-		} //Fim do bloco do Bubble
-
-		if (strcmp(argv[1], "-i") == 0) {
-			printf("\nInsertion:\n");
-			v = (int*) malloc(n * sizeof(int));
-			insertion(v, n);
-		}
-		if (strcmp(argv[1], "-m") == 0) {
-			v = (int*) malloc(n * sizeof(int));
-			printf("\nMerge:\n");
-			merge(v, 0, n - 1);
-		}
-		if (strcmp(argv[1], "-q") == 0) {
-			v = (int*) malloc(n * sizeof(int));
-			printf("\nQuick:\n");
-			quick(v, 0, n - 1);
-		}
-		if (strcmp(argv[1], "-h") == 0) {
-			v = (int*) malloc(n * sizeof(int));
-			printf("\nHeap:\n");
-			heap(v, n);
-		}
-		if (strcmp(argv[1], "-g") == 0) {
-			printf("\nGera Vetores:\n");
-			geraBubble();
-			geraAleatorios();
-			geraInsertion();
-			break;
-		}
-
-		printf("Depois %d:\n", i + 1);
-		printV(v, n);
-		free(v);
-		i++;
-
-	}
-
-	infile.close();
-
-	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-	cout << "printf: " << duration << '\n';
 
 	return EXIT_SUCCESS;
 }
